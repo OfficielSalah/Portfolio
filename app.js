1ef8f2cbf50dc764df1ac8c207a49929b9d65d08
OfficielSalah <officielsalah1970@gmail.com>
2020-04-02 02:47:08 +0100

    adding bootstrap for responsive website

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-window.onscroll = function() {stickbar()};
-
-var navbar = document.getElementById("nav2");
-var sticky = navbar.offsetTop;
-
-function stickbar() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    navbar.style.opacity=1.0;
+var navbar1 = document.getElementById("nav2");
+var sticky1 = navbar1.offsetTop;
+function stickbar1() {
+  if (window.pageYOffset + 50 >= sticky1) {
+    navbar1.classList.add("sticky");
+    navbar1.style.opacity = 1.0;
   } else {
-    navbar.classList.remove("sticky");
-    navbar.style.opacity=0.0;
+    navbar1.classList.remove("sticky");
+    navbar1.style.opacity = 0.0;
+  }
+}
+var navbar2 = document.getElementById("cont");
+var navbar3 = document.getElementById("ski");
+var sticky2 = navbar2.offsetTop;
+
+function stickbar2() {
+  if (window.pageYOffset - 900 >= sticky2) {
+    navbar3.classList.remove("active");
+    navbar2.classList.add("active");
+  } else {
+    navbar3.classList.add("active");
+    navbar2.classList.remove("active");
   }
 }
 
+window.onscroll = function() {
+  stickbar1();
+  stickbar2();
+};
